ecd3e85d371af26020f56bb99e7751ed1f92937a
Shawal <shahishawal@gmail.com>
2023-08-04 16:37:10 +0600

    prblm-13

---
+++ problem13.js
@@ -0,0 +1,10 @@
+//problem: ekti function likte hbe  function er nam hbe takaToDollar .ekn tomake ekti sonkka nite hbe  perameter hisebe za tk k dollar e convert kore return krte hbe  ebong kto hy seta console e deka te hbe
+
+function takaToDollar(num) {
+    const dollar= num / 110;
+    return dollar;
+}
+
+const taka=33320;
+const result=takaToDollar(taka);
+console.log(result .toFixed(2));
