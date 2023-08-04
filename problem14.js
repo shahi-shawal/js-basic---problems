480a6be6f06cdf7ce3566e07cebe7ab0a4ae7d72
Shawal <shahishawal@gmail.com>
2023-08-04 17:22:12 +0600

    prb14

---
+++ problem14.js
@@ -0,0 +1,22 @@
+//problem: Tumi ekta function banabe jar nam hbe barbieKen .eta input hisebe ekta string nibe. task hcce function e string e total koita character  ache tar opor vitti kre console output korbe .ekn lenth jdi even hy out put hbe hi ken ar odd hle hi barbie 
+
+function barbieKen(string) {
+     let totalCharacter= string.length;
+     console.log("Total Character is "+ totalCharacter); 
+
+     if (totalCharacter%2==0) {
+       console.log("Hi ken");
+     }
+     else{
+        console.log("Hi barbie");
+
+     }
+}
+
+const stg="Shawalshahi";
+const result= barbieKen(stg);
+
+ 
+//  output:Total Character is 11
+//         Hi barbie
+  
